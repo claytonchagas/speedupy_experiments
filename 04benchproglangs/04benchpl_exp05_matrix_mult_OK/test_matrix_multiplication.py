e97d151846e3108db78dce7d1f4c761032d579f5
lanerson <45043973+lanerson@users.noreply.github.com>
2023-08-03 21:37:46 -0300

    Update test_matrix_multiplication.py

--- 04benchproglangs/04benchpl_exp05_matrix_mult_OK/test_matrix_multiplication.py
+++ 04benchproglangs/04benchpl_exp05_matrix_mult_OK/test_matrix_multiplication.py
@@ -5,7 +5,7 @@ import sys
 
 import time
 
-from intpy.intpy import initialize_intpy, deterministic
+from intpy import initialize_intpy, deterministic
 
 #--------------------------------
 # Function: matrix_multiplication
