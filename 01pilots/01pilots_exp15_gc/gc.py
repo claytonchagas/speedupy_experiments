0947937398adacc48e0e2b13b9d398db64e06c0c
lanerson <45043973+lanerson@users.noreply.github.com>
2023-08-25 17:46:31 -0300

    Update gc.py

--- 01pilots/01pilots_exp15_gc/gc.py
+++ 01pilots/01pilots_exp15_gc/gc.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import sys
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
-from intpy.intpy import initialize_intpy, deterministic
+from intpy import initialize_intpy, deterministic
 
 import time
 import sys
