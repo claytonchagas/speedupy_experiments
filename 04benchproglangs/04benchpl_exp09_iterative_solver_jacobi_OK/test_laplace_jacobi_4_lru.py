ce0b3cb1aab58419b48c2f2089abe1d9b443fa7a
lanerson <45043973+lanerson@users.noreply.github.com>
2023-08-28 17:22:37 -0300

    Update test_laplace_jacobi_4_lru.py

--- 04benchproglangs/04benchpl_exp09_iterative_solver_jacobi_OK/test_laplace_jacobi_4_lru.py
+++ 04benchproglangs/04benchpl_exp09_iterative_solver_jacobi_OK/test_laplace_jacobi_4_lru.py
@@ -1,10 +1,10 @@
-import sys
+from functools import lru_cache
+import sys, time
 
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / "speedupy"))
 
 import numpy as np
-import time
 
 @lru_cache(maxsize=100)
 def loop_time_step(u):
