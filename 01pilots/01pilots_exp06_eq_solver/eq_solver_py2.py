f746b5080ea7e7412d2fc44c487889dd386f61ab
lanerson <45043973+lanerson@users.noreply.github.com>
2023-08-28 17:07:26 -0300

    Update eq_solver_py2.py

--- 01pilots/01pilots_exp06_eq_solver/eq_solver_py2.py
+++ 01pilots/01pilots_exp06_eq_solver/eq_solver_py2.py
@@ -21,11 +21,9 @@ n_solutions = 200
 x_list = np.linspace(1, 2e4, n_solutions)
 F_list = np.empty_like(x_list)
 
-@deterministic
 def solve(n, F_solution):
     return fsolve(func=lambda x: n * C / (1 + Kd1 / x) + n2 * C / (1 + Kd2 / x) - T, x0=F_solution, xtol=1e-6)
 
-@initialize_intpy(__file__)
 def main(F_solution):
     for i in range(n_solutions):
         # Define function
