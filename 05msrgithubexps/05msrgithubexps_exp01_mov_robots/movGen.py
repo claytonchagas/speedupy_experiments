f47d89fc875a631c4343e0d0223f14431c0b7f00
lanerson <45043973+lanerson@users.noreply.github.com>
2023-09-07 19:19:05 -0300

    Create movGen.py

---
+++ 05msrgithubexps/05msrgithubexps_exp01_mov_robots/movGen.py
@@ -0,0 +1,45 @@
+from numpy import linspace
+from random import randint
+import sys
+
+l1 = [
+    "X_coordinates_left_wheel_left = ",
+    "Y_coordinates_left_wheel_left = ",
+    "X_coordinates_right_wheel_left = ",
+    "Y_coordinates_right_wheel_left = ",
+]
+
+l2 = [
+    "X_coordinates_left_wheel_right = ",
+    "Y_coordinates_left_wheel_right = ",
+    "X_coordinates_right_wheel_right = ",
+    "Y_coordinates_right_wheel_right = ",
+]
+
+l3 = [
+    "X_coordinates_left_wheel_straight = ",
+    "Y_coordinates_left_wheel_straight = ",
+    "X_coordinates_right_wheel_straight = ",
+    "y_coordinates_right_wheel_straight = ",
+]
+
+names = {
+    "leftWheelPositions.csv": l1,
+    "rightWheelPositions.csv": l2,
+    "straightWheelPositions.csv": l3,
+}
+
+
+def MovGen(n=100):
+    for it in names:
+        with open("./" + it, "w") as file:
+            for t in names[it]:
+                n1, n2 = randint(1, 20), randint(50, 100)
+                movs = str(list(linspace(n1, n2, n).round(1)))[1:-2]
+                text = t + movs
+                file.write(text + "\n")
+
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    MovGen(n)
