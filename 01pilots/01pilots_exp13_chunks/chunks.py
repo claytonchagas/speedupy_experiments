fc2db0c89ac1bd92835a2e64ce04b5be354b64da
lanerson <45043973+lanerson@users.noreply.github.com>
2023-10-05 15:09:32 -0300

    Update chunks.py

--- 01pilots/01pilots_exp13_chunks/chunks.py
+++ 01pilots/01pilots_exp13_chunks/chunks.py
@@ -1,9 +1,9 @@
-from intpy import initialize_intpy, deterministic
 import time, sys
 import itertools
 
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / "speedupy"))
+from intpy import initialize_intpy, deterministic
 
 @deterministic
 def chunks(l):
